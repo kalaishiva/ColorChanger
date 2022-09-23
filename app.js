a15bfc0ec4cfb9b4306b0095d71820aced3b6551
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-23 12:03:41 +0200

    background color javascript project completed

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 //#ff09ff
 let alphNum = "ABCDEF0123456789";
 //console.log(alphNum[0]); // A
-let colorCode = "#";
 
 
 
 //console.log(alphNum.split(""));
 
-for (let i = 0; i < 6; i++) {
-    let randomNum = Math.floor(Math.random() * 16);
-    //console.log(randomNum);
-    colorCode += alphNum[randomNum];
-    console.log(colorCode); //#02C98E
+
+
+//button
+const button = document.getElementById("button");
+console.log(button);
+
+//background color
+function changecolor() {
+    let colorCode = "#";
+    const bgColor = document.getElementById("canvas");
+    for (let i = 0; i < 6; i++) {
+        let randomNum = Math.floor(Math.random() * 16);
+        //console.log(randomNum);
+        colorCode += alphNum[randomNum];
+        console.log(colorCode); //#02C98E
+    }
+    bgColor.style.backgroundColor = colorCode;
 }
 
-const button = document.getElementById("button");
+button.addEventListener("click", changecolor)
